2c182d75ce2941aadc5685807fc0c28c5a88925d
DIIMIIM <emilian@stater.co>
2025-04-19 23:09:19 +0300

    feat: simplified the repo a bit

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import { Suspense } from 'react';
-import { getAll } from '../data/db';
-import Like from './Like';
 
 async function Albums() {
-  const albums = await getAll();
   return (
     <ul>
-      {albums.map((a) => (
+      {/*albums.map((a) => (
         <li key={a.id} className="flex gap-2 items-center mb-2">
           <img className="w-20 aspect-square" src={a.cover} alt={a.title} />
           <div>
@@ -15,7 +12,7 @@ async function Albums() {
             <Like />
           </div>
         </li>
-      ))}
+      ))*/}
     </ul>
   );
 }
